e0c845e4d37c4c9b6360ca8e8d28b5dc5765b703
vamsideepak <vamsideepak123@gmail.com>
2018-10-08 19:05:32 +0530

    added smartcard library pcsc

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -3,9 +3,109 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 import { GameService } from '../../game.service';
 import { ActivatedRoute, Router } from '@angular/router';
+//import { NFC } from 'nfc-pcsc';
+//import {pcsc} from 'pcsclite';
 declare var WebCamera: any;
 declare var dialog: any;
 declare var fs: any;
+//declare var NFC: any;
+ declare var pcsc: any;
+ var pcs = pcsc();
+
+
+pcs.on('reader', function(reader) {
+
+  console.log('New reader detected', reader.name);
+
+  reader.on('error', function(err) {
+      console.log('Error(', this.name, '):', err.message);
+  });
+
+  reader.on('status', function(status) {
+      console.log('Status(', this.name, '):', status);
+      /* check what has changed */
+      const changes = this.state ^ status.state;
+      if (changes) {
+          if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+              console.log("card removed");/* card removed */
+              reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+                  if (err) {
+                      console.log(err);
+                  } else {
+                      console.log('Disconnected');
+                  }
+              });
+          } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+              console.log("card inserted");/* card inserted */
+              reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+                  if (err) {
+                      console.log(err);
+                  } else {
+                      console.log('Protocol(', reader.name, '):', protocol);
+                      reader.transmit(new Buffer([0x00, 0xB0, 0x00, 0x00, 0x20]), 40, protocol, function(err, data) {
+                          if (err) {
+                              console.log(err);
+                          } else {
+                              console.log('Data received', data);
+                              reader.close();
+                              pcs.close();
+                          }
+                      });
+                  }
+              });
+          }
+      }
+  });
+
+  reader.on('end', function() {
+      console.log('Reader',  this.name, 'removed');
+  });
+});
+
+pcs.on('error', function(err) {
+  console.log('PCSC error', err.message);
+});
+/*
+@SMART CODE READING CODE STARTING FROM HERE
+*/
+// const nfc = new NFC();
+
+// nfc.on('reader', reader => {
+ 
+//   console.log(`${reader.reader.name}  device attached`);
+
+//   // needed for reading tags emulated with Android HCE
+//   // custom AID, change according to your Android for tag emulation
+//   // see https://developer.android.com/guide/topics/connectivity/nfc/hce.html
+//   reader.aid = 'F222222222';
+
+//   reader.on('card', card => {
+
+//       console.log(`${reader.reader.name}  card detected`, card);
+
+//   });
+
+//   reader.on('card.off', card => {
+//       console.log(`${reader.reader.name}  card removed`, card);
+//   });
+
+//   reader.on('error', err => {
+//       console.log(`${reader.reader.name}  an error occurred`, err);
+//   });
+
+//   reader.on('end', () => {
+//       console.log(`${reader.reader.name}  device removed`);
+//   });
+
+// });
+
+// nfc.on('error', err => {
+//   console.log('an error occurred', err);
+// });
+
+/*
+@SMART CODE READING CODE END HERE
+*/
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -86,6 +186,9 @@ export class CreateComponent implements OnInit {
       console.log("Please enable the camera first to take the snapshot !");
   }
   }
+
+//smart card code 
+
 
   
   ngOnInit() {
